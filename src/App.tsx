ad38430b9613c0a078f9b13126c26a1b1faf7ae1
Phillip Han <phillip.han05@gmail.com>
2026-08-06 14:11:25 +1000

    Restore english sample

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import PageTransition from "@/components/animations/PageTransition";
 import ScrollProgress from "@/components/animations/ScrollProgress";
 import StickyBookButton from "@/components/StickyBookButton";
 import Index from "./pages/Index";
+import EnglishSample from "./pages/EnglishSample";
 import BookInterview from "./pages/BookInterview";
 import Reviews from "./pages/Reviews";
 import NotFound from "./pages/NotFound";
@@ -62,7 +63,8 @@ const AnimatedRoutes = () => {
     <AnimatePresence mode="wait">
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<PageTransition><Index /></PageTransition>} />
-        <Route path="/english-sample" element={<PageTransition><Index /></PageTransition>} />
+        {/* Do NOT change this to <Index />. See src/pages/EnglishSample.tsx header comment. */}
+        <Route path="/english-sample" element={<PageTransition><EnglishSample /></PageTransition>} />
         <Route
           path="/book-intro-calibration"
           element={import.meta.env.DEV ? <BookIntroCalibration /> : <Navigate to="/" replace />}
